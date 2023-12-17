2ff4631308f83f51033059c788d5703e15f5a994
althafnash <althafnash14@gmail.com>
2023-12-17 22:54:15 +0400

    ed to make a file server but failed

--- app.js
+++ app.js
@@ -5,33 +5,34 @@ import  chalk  from "chalk"
 import { opreating } from "./lib/Home.js";
 import { Help } from "./lib/Help.js"
 
-welcome();
-const answer = await inquirer.prompt({
-    name: "command",
-    type: "input",
-    message: chalk.white("command : "),
-});
-const command = answer.command;
-if(command == "os"){
-    console.log(chalk.green("Opreating System Information : "))
-    await opreating();
-} 
-else if(command == "help"){
-    console.log(chalk.green("Help_--_Deck"));
-    await Help()
+while(true) {
+    welcome();
+    const answer = await inquirer.prompt({
+        name: "command",
+        type: "input",
+        message: chalk.white("command : "),
+    });
+    const command = answer.command;
+    if(command == "os"){
+        console.log(chalk.green("Opreating System Information : "))
+        await opreating();
+    } 
+    else if(command == "help"){
+        console.log(chalk.green("Help_--_Deck"));
+        await Help()
+    }
+    else if(command == "commands"){
+        console.log(chalk.green("======================================================"))
+        console.log(chalk.green(" help -- The Help_Deck"))
+        console.log(chalk.green(" os   -- opreating system information"))
+        console.log(chalk.green("======================================================"))
+    }else {
+        break
+    }
 }
-else if(commands == "commands"){
-    console.log(chalk.green("======================================================"))
-    console.log(chalk.green(" help -- The Help_Deck"))
-    console.log(chalk.green(" os   -- opreating system information"))
-    console.log(chalk.green("======================================================"))
-}
-
-
-
-function welcome() {
-    console.log(chalk.white("Hello welcome to Nash CMD "))
-    console.log(chalk.red("Type commands to see all the commands "))
-    console.log(chalk.green("Close the window to exit"))
-}
-
+   
+ function welcome() {
+        console.log(chalk.white("Hello welcome to Nash CMD "))
+        console.log(chalk.red("Type commands to see all the commands "))
+        console.log(chalk.green("Close the window to exit"))
+    }
